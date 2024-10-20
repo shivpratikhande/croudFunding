ec7f5a391f157cb2ac833400271efb0a9595c927
shivv <shivpratikhande2017@gmail.com>
2024-10-20 10:21:52 +0530

    update with the new one

--- frontend/src/client.ts
+++
@@ -1,9 +0,0 @@
-import { createThirdwebClient } from "thirdweb";
-
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = "9bcf5d2f319d5a16711a992331b89f67"
-
-export const client = createThirdwebClient({
-  clientId: clientId,
-});
--- frontend/src/pages/CampaignDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CampaignDetails() {
-  return (
-    <div>CampaignDetails</div>
-  )
-}
-
-export default CampaignDetails
